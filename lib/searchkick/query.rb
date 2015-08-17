658bd7a6d4e1d965485af82c2578a27480dd2c39
Karthik Mallavarapu <karthik.mallavarapu@gmail.com>
2015-08-17 16:20:32 +0530

    Adds match phrase option to searchkick

--- lib/searchkick/query.rb
+++ lib/searchkick/query.rb
@@ -118,7 +118,11 @@ module Searchkick
                 qs << shared_options.merge(analyzer: analyzer)
               end
 
-              queries.concat(qs.map { |q| {match: {field => q}} })
+              # XXX: Commented line 122..added lines 123 and 124..revert back
+              # Based on https://github.com/pluton-house/searchkick
+              #queries.concat(qs.map { |q| {match: {field => q}} })
+              match_type = options[:match_phrase] == true ? :match_phrase : :match
+              queries.concat(qs.map { |q| {match_type => {field => q}} })
             end
 
             payload = {
